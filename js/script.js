3f1d3650bb3e21b943aad444775d313ffb9ccad2
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-03-28 14:56:06 +0000

    I finished most of the website

--- js/script.js
+++ js/script.js
@@ -8,28 +8,22 @@
 
 function enterClicked() {
   // get the user work hours
-  let length= parseFloat(document.getElementById("prism-length").value)
+  let aBase= parseFloat(document.getElementById("prism-base-a").value)
 
 	// get the hourly pay from the user
   let height= parseFloat(document.getElementById("prism-height").value)
 
   // get the hourly pay from the user
-  let width= parseFloat(document.getElementById("prism-width").value)
+  let bBase= parseFloat(document.getElementById("prism-base-b").value)
 
 
   //calculate the pay  
-  let volume = length*height*width;
+  let abBase = aBase+bBase;
   //calculate the taxes
-  let widthLength = 2*width*length;
-  let heightLength = 2*height*length;
-  let heightWidth = 2*height*width;
-  let surfaceArea = widthLength+heightLength+heightWidth
+  let area =  abBase*0.5*height
 
 
   // display the user's pay
-  
-  document.getElementById('answer1').innerHTML = "The volume of your rectangular prism is " +volume.toFixed(2)+"cm<sup>3</sup>";
-  // display how much the government will pay
-  document.getElementById('answer2').innerHTML = "The surface area of your rectangular prism is "+surfaceArea.toFixed(2)+"cm<sup>2</sup>"; 
+  document.getElementById('answer1').innerHTML = "The area of your trapezoid is " +area.toFixed(2)+"cm<sup>2</sup>";
 }
 
